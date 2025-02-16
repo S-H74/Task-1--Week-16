2642951055e4c7370ac41a53d0dba993322f8bfc
S-H74 <Gethub770@gmail.com>
2025-02-16 18:34:14 +0200

    Task-1--Week-16

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-about',
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css',
+})
+export class AboutComponent {
+
+}
